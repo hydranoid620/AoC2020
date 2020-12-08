1adcaf454598ff0b7dded0cd8920bb19e0ff0553
Nicholas Rosati <nicholas@rosati.xyz>
2020-12-07 23:56:21 -0500

    Day 2 Part 1

--- Day2/main.ts
+++ Day2/main.ts
@@ -1,10 +1,49 @@
 //  Input format:
 //      <min occurrences>-<max occurrences> <char>: <password>
 
-import {readFileSync} from 'fs';
+export function main(input: string): number {
+    let lineData: LineData[] = [];
+    input.split("\n").forEach((element) => lineData.push(parseLine(element)));
 
-export function main(input: string) {
+    let validPasswords: number = 0;
+    lineData.forEach((element) => {
+        let count: number = element.password.split(element.character).length - 1;
+        if (count >= element.min && count <= element.max) validPasswords += 1;
+    });
 
+    return validPasswords;
+}
+
+interface LineData {
+    min: number,
+    max: number,
+    character: string,
+    password: string
+}
+
+function parseLine(input: string): LineData {
+    return {
+        min: getMinNumber(input),
+        max: getMaxNumber(input),
+        character: getCharacter(input),
+        password: getPassword(input)
+    }
+}
+
+function getMinNumber(line: string): number {
+    return Number(line.split("-", 1)[0]);
+}
+
+function getMaxNumber(line: string): number {
+    return Number(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
+}
+
+function getCharacter(line: string): string {
+    return line.substring(line.indexOf(" ") + 1, line.indexOf(":"));
+}
+
+function getPassword(line: string): string {
+    return line.split(" ")[2].replace("\r", "");
 }
 
 export { main as Day2 };
