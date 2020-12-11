10b62277d2f4497203f3f9ab844f75f8974f90e0
Nicholas Rosati <nicholas@rosati.xyz>
2020-12-11 00:36:54 -0500

    Day 3 part 2

--- Day3/main.ts
+++ Day3/main.ts
@@ -7,15 +7,53 @@ export function part1(input: string): number {
     let column: number = 0;
 
     input.split("\n").forEach((line) => {
-        if (line.charAt(column) === "#") treeHits++;
+        if (line.charAt(column % width) === "#") treeHits++;
         column += 3;
-        if (column >= width) column = column - width;
     });
 
     return treeHits;
 }
 
+//Yucky
 export function part2(input: string): number {
+    const width: number = input.indexOf("\n");
+    const lineData: string[] = input.split("\n");
+    
+    let treeHits: number = 0;
+    let currentColumn: number = 0;
+    let currentTreeHits: number = 0;
 
-    return -1;
+    lineData.forEach(line => {
+        if (line.charAt(currentColumn % width) === '#') currentTreeHits++;
+        currentColumn += 1;
+    });
+    treeHits += currentTreeHits;
+    currentColumn = 0;
+    currentTreeHits = 0;
+
+    treeHits *= part1(input); //This one is the same
+
+    lineData.forEach(line => {
+        if (line.charAt(currentColumn % width) === '#') currentTreeHits++;
+        currentColumn += 5;
+    });
+    treeHits *= currentTreeHits;
+    currentColumn = 0;
+    currentTreeHits = 0;
+
+    lineData.forEach(line => {
+        if (line.charAt(currentColumn % width) === '#') currentTreeHits++;
+        currentColumn += 7;
+    });
+    treeHits *= currentTreeHits;
+    currentColumn = 0;
+    currentTreeHits = 0;
+
+    for (let i = 0; i < lineData.length; i += 2) {
+        if (lineData[i].charAt(currentColumn % width) === '#') currentTreeHits++;
+        currentColumn += 1;
+    }
+    treeHits *= currentTreeHits;
+
+    return treeHits;
 }
